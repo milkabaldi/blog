81d9893888f94e54e309c9ad77ef6c36d805f005
milkabaldi <justmainc8@gmail.com>
2023-01-29 18:21:20 +0200

    Database

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home({title}) {
+export default function Home({data}) {
   return (
     <>
       <Head>
@@ -23,28 +23,11 @@ export default function Home({title}) {
           </nav>
       </header>
       <main className={styles.main}>
-        <h1>{title}</h1>
-        <a href=''>
-          <img src="" alt="City" />
-          <h2>Events in London</h2>
-          <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-          </p>
-        </a>
-        <a href=''>
-          <img src="" alt="City" />
-          <h2>Events in San Diego</h2>
-          <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-          </p>
-        </a>
-        <a href=''>
-          <img src="" alt="City" />
-          <h2>Events in Kyiv</h2>
-          <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-          </p>
-        </a>
+      <h1>Events pages</h1>
+
+      {data.map(ev => <a key={ev.id} href={`/events/${ev.id}`}><Image width={200} height={'100%'} alt={ev.title} src={data.image} /><h2>{ev.title}</h2><p>{ev.description}</p></a> )}
+
+
       </main>
       <footer className={styles.footer}>
         <p>Blog 2023 Всі права захищені</p>
@@ -57,10 +40,13 @@ export default function Home({title}) {
 
 
 
-export function getServerSideProps(){
+export async function getServerSideProps(){
+  const {events_categories} = await import('/data/data.json');
+  console.log(events_categories)
+
   return {
     props:{
-      title: 'Hellow everyone'
+      data: events_categories,
     }
   }
 }
