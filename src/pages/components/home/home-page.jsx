7f91233b142b15e1ee7f535662d41c65e816e51f
milkabaldi <justmainc8@gmail.com>
2023-02-03 18:22:46 +0200

    Changes in cards

--- src/pages/components/home/home-page.jsx
+++ src/pages/components/home/home-page.jsx
@@ -4,10 +4,12 @@ import Image from "next/image"
 export const HomePage = ({data}) => 
      (
         <div className="home__body">
-        <h1>Home pages</h1>
-        
-        {data.map(ev => <Link className="hpme__body-card" key={ev.id} href={`/events/${ev.id}`} passHref>
-            <div className='image'><Image width={200} height={200} alt={ev.title} src={ev.image} /></div><h2>{ev.title}</h2><p>{ev.description}</p></Link> )}
+        {data.map(ev => <Link className="home__body-card" key={ev.id} href={`/events/${ev.id}`} passHref>
+            <div className='image'>
+                <Image width={200} height={200} alt={ev.title} src={ev.image} />
+            </div>
+                <h2>{ev.title}</h2>
+                <p>{ev.description}</p></Link> )}
         </div>
     )
 
