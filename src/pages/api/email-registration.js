59698e74684b039d80ca1ae6a3da789eb9df5285
milkabaldi <justmainc8@gmail.com>
2023-02-06 12:44:09 +0200

    Email reg post

---
+++ src/pages/api/email-registration.js
@@ -0,0 +1,15 @@
+export default function handler(req, res) {
+  const { method } = req
+  if (method === 'POST') {
+    const { email, eventId } = req.body
+
+    //we add our code here
+
+    res
+      .status(200)
+      .json({
+        message:
+          'You has been registered successfully with the email: ${email} ${eventId}',
+      })
+  }
+}
--- src/pages/components/events/single-event.jsx
+++ src/pages/components/events/single-event.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
-const SingleEvent = ({data}) => {
+const SingleEvent = ({ data }) => {
+  const inputEmail = useRef()
+  const router = useRouter()
+  const onSubmit = async(e) => {
+    e.preventDefault()
+    const emailValue = inputEmail.current.value
+    const eventId = router?.query.id
+
+    try{
+      const response = await fetch('/appi/email-registration', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({email: emailValue, eventId: eventId})
+      })
+      if(!response.ok) throw new Error (`Error: ${response.status}`)
+      const data = await response.json()
+
+      //POST fetch request
+      // body emailValue and the eventId
+    } catch (e) {
+      console.log('ERROR', e)
+    }
+
+  }
+
   return (
-    <div className='single__page'>
-    <h1>{data.title}</h1>
-    <Image src={data.image} width={1000} height={500} alt={data.title} />
-    <p>{data.description}</p>
-    <form onSubmit={onSubmit} className='single__page-email'></form>
-    <label>Get Registered for this event!</label>
-    <input type="email " id='email' placeholder='Please insert your email here' /> <button type='submit'>Submit</button>
-</div>
+    <div className="single">
+      <h1>{data.title}</h1>
+      <Image src={data.image} width={1000} height={500} alt={data.title} />
+      <p>{data.description}</p>
+      <form onSubmit={onSubmit} className="single__email">
+      <label>Get Registered for this event!</label>
+      <input
+        ref={inputEmail}
+        type="email "
+        id="email"
+        placeholder="Please insert your email here"
+      />
+      <button type="submit">Submit</button>
+      </form>
+
+    </div>
   )
 }
 
